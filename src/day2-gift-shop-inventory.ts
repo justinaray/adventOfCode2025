a97d3678a4235ad6f8fcc54fe88cfdfe6e301f8b
justinaray <justin.a.ray@gmail.com>
2025-12-10 20:34:27 -0500

    Ran default formatter on day 2

--- src/day2-gift-shop-inventory.ts
+++ src/day2-gift-shop-inventory.ts
@@ -1,71 +1,72 @@
-import { readFileSync } from "fs";
-import { assertNever } from "./ts-utils";
+import { readFileSync } from 'fs';
+import { assertNever } from './ts-utils';
 
-const sampleInput = '11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124';
+const sampleInput =
+  '11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124';
 
 type DetectionType = 'simple' | 'advanced';
 type ProductRange = {
-	start: number;
-	end: number;
-}
-function parseProductRanges(input:string):ProductRange[] {
-	const ranges = input.split(',').map(range => {
-		const [start, end] = range.split('-');
-		return { start: parseInt(start), end: parseInt(end) };
-	});
-
-	return ranges;
+  start: number;
+  end: number;
 };
+function parseProductRanges(input: string): ProductRange[] {
+  const ranges = input.split(',').map(range => {
+    const [start, end] = range.split('-');
+    return { start: parseInt(start), end: parseInt(end) };
+  });
+
+  return ranges;
+}
 
 /**
  * For part 1 of the coding challenge, a simple invalid id is a number
  * containing an evenly split and duplicated sequence of digits.
- * 
+ *
  * @param id the product number
  * @returns true if the product contains an evenly split and duplicated sequence
- * of digits (e.g. 11, 22, 1010, 222222, 1188511885); false otherwise 
+ * of digits (e.g. 11, 22, 1010, 222222, 1188511885); false otherwise
  */
-function isSimpleInvalidProductId(id:number):boolean {
-	const digits = id.toString();
-	if (digits.length === 0 || digits.length % 2 === 1) {
-		// empty or odd lengths can't be duplications
-		return false;
-	}
+function isSimpleInvalidProductId(id: number): boolean {
+  const digits = id.toString();
+  if (digits.length === 0 || digits.length % 2 === 1) {
+    // empty or odd lengths can't be duplications
+    return false;
+  }
 
-	const midpointIndex = digits.length / 2;
-	return digits.slice(0, midpointIndex) === digits.slice(midpointIndex); 
+  const midpointIndex = digits.length / 2;
+  return digits.slice(0, midpointIndex) === digits.slice(midpointIndex);
 }
 
 /**
  * For part 2 of the coding challenge, an invalid id is a number
  * containing only sequences of duplicated digits
- * 
+ *
  * @param id the product number
  * @returns true if the product contains at least 2 duplicated sequences of digits
- * (e.g. 11, 22, 111, 1010, 446446, 1111111, 2121212121); false otherwise 
+ * (e.g. 11, 22, 111, 1010, 446446, 1111111, 2121212121); false otherwise
  */
-function isComplexInvalidProductId(id:number):boolean {
-	const digitsStr = id.toString();
-	const digits = digitsStr.split('');
+function isComplexInvalidProductId(id: number): boolean {
+  const digitsStr = id.toString();
+  const digits = digitsStr.split('');
 
-	if (digitsStr.length === 1) {
-		return false;
-	}
+  if (digitsStr.length === 1) {
+    return false;
+  }
 
-	const maxRepeatLength = digits.length / 2;
+  const maxRepeatLength = digits.length / 2;
 
-	for (let i = 0; i < maxRepeatLength; i++) {
-		const currSeqToCheck = digits.slice(0, i + 1).join('');
-		const numRepeats = digitsStr.length / currSeqToCheck.length;
-		if (!Number.isInteger(numRepeats)) {
-			continue;
-		}
-		if (currSeqToCheck.repeat(numRepeats) === digitsStr) {
-			return true;
-		}
-	}
+  for (let i = 0; i < maxRepeatLength; i++) {
+    const currSeqToCheck = digits.slice(0, i + 1).join('');
+    const numRepeats = digitsStr.length / currSeqToCheck.length;
+    if (!Number.isInteger(numRepeats)) {
+      continue;
+    }
+    if (currSeqToCheck.repeat(numRepeats) === digitsStr) {
+      return true;
+    }
+  }
 
-	return false;
+  return false;
 }
 
 function findInvalidProductIds(
@@ -94,24 +95,24 @@ function findInvalidProductIds(
 }
 
 function runSampleInput(detectionType: DetectionType = 'simple') {
-	const invalidProductIds = parseProductRanges(sampleInput).flatMap(range => {
-		return findInvalidProductIds(range, detectionType);
-	});
-	const sum = invalidProductIds.reduce<number>((sum, curr) => {
-		return sum += curr;
-	}, 0);
-	console.log('Sample input', detectionType, sum, invalidProductIds.length);
+  const invalidProductIds = parseProductRanges(sampleInput).flatMap(range => {
+    return findInvalidProductIds(range, detectionType);
+  });
+  const sum = invalidProductIds.reduce<number>((sum, curr) => {
+    return (sum += curr);
+  }, 0);
+  console.log('Sample input', detectionType, sum, invalidProductIds.length);
 }
 
 function runFullInput(detectionType: DetectionType = 'simple') {
-	const fullInput = readFileSync('./inputs/day2-gift-shop-inventory.txt', 'utf8').trim();
-	const invalidProductIds = parseProductRanges(fullInput).flatMap(range => {
-		return findInvalidProductIds(range, detectionType);
-	});
-	const sum = invalidProductIds.reduce<number>((sum, curr) => {
-		return sum += curr;
-	}, 0);
-	console.log('Full input', detectionType, sum, invalidProductIds.length);
+  const fullInput = readFileSync('./inputs/day2-gift-shop-inventory.txt', 'utf8').trim();
+  const invalidProductIds = parseProductRanges(fullInput).flatMap(range => {
+    return findInvalidProductIds(range, detectionType);
+  });
+  const sum = invalidProductIds.reduce<number>((sum, curr) => {
+    return (sum += curr);
+  }, 0);
+  console.log('Full input', detectionType, sum, invalidProductIds.length);
 }
 
 // runSampleInput('simple');
